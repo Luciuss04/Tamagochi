58b607dc454fcc8d6517f10349b68c77642f543e
Josep <luciuss0444@gmail.com>
2026-01-07 20:20:33 +0100

    Ignorar archivos de compilación y configuración del IDE

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,37 @@
 package org.example;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+    public static void main(String[] args) {
+        System.out.println("Hola Git");
     }
+}
+private static int menuPrincipal() {
+    Scanner sc = new Scanner(System.in);
+    int opcion;
+
+    do {
+        System.out.println("1. Estado");
+        System.out.println("2. Comer");
+        System.out.println("3. Jugar");
+        System.out.println("4. Dormir");
+        System.out.println("----------------");
+        System.out.println("0. Salir");
+        System.out.print("Seleccione una opción del menú: ");
+
+        while (!sc.hasNextInt()) {
+            System.out.println("Opción no válida. Introduzca un número.");
+            sc.next(); // descartar entrada incorrecta
+            System.out.print("Seleccione una opción del menú: ");
+        }
+
+        opcion = sc.nextInt();
+
+        if (opcion < 0 || opcion > 4) {
+            System.out.println("Opción no válida. Inténtelo de nuevo.\n");
+        }
+
+    } while (opcion < 0 || opcion > 4);
+
+    return opcion;
 }
